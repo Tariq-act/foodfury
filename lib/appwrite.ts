c36ed091038976863a1b3ea33234adb5bc2a8c79
Tariq-act <khantarikact@gmail.com>
2025-08-15 20:10:35 +0530

    04: Auth screen

---
+++ lib/appwrite.ts
@@ -0,0 +1,7 @@
+export const appwriteConfig = {
+  endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+  platform: "com.act.foodordering",
+  projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+  databaseId: "689cca1c0002dde18466",
+  userCollectionId: "689ccf50002cc9f39754",
+};
